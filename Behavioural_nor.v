7e7e7626362e43992d1108b1a20f0586b9594d55
Komali <97676884+KomaliKomali@users.noreply.github.com>
2024-03-12 05:02:21 -0500

    Behavioural_nor.v

--- Behavioural_nor.v
+++ Behavioural_nor.v
@@ -1,11 +1,13 @@
 module behavioural(A,B,Y);
   input A,B;
   output Y;
-  reg Y;
-  
-always @ (A or B)
+  reg Y;  
+
+////In Behavioural model "Always block and Case statements" will be used
+              
+always @ (A or B) 
  begin
-   if (A == 1'b0 & B == 1'b0)
+   if (A == 1'b0 & B == 1'b0)   ////we will specify the behaviour of the output corresponding to the input
     begin
      Y = 1'b1;
     end
