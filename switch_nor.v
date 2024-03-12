6e26e619980910936287d49105a0165b1df3a0f6
Komali <97676884+KomaliKomali@users.noreply.github.com>
2024-03-12 05:04:21 -0500

    switch_nor.v

--- switch_nor.v
+++ switch_nor.v
@@ -3,12 +3,13 @@ module circuit_level(Y,A,B);
  input A,B;
  wire C;
  
- supply1 pwr;
- supply0 gnd;
+ supply1 pwr;      ///supply1 is a Net data type which is used to model the power 
+ 
+ supply0 gnd;     ///supply0 is a Net data type which is used to model the ground
 
-pmos(C,pwr,B);
+pmos(C,pwr,B);    ///pmos is already available as a primitive
 pmos(Y,C,A);
-nmos(Y,gnd,A);
+nmos(Y,gnd,A);    ///nmos is already available as a primitive
 nmos(Y,gnd,B);
 
 endmodule 
