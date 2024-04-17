502e7e29f83fb17596cc51aa39f911bd8026b619
Komali <97676884+KomaliKomali@users.noreply.github.com>
2024-04-17 18:22:19 +0530

    Add files via upload

---
+++ Day_37/Ring_counter/Test_bench/Ring_counter_tb.v
@@ -0,0 +1,46 @@
+ module Ring_counter_tb();
+  reg clk,rst;
+  wire [3:0]count;
+ 
+ ///instantiation
+  ring_counter RC_tb(clk,rst,count);
+ 
+ ////clock
+  initial
+   begin
+   clk = 1'b0;
+   forever
+   #5 clk = ~clk;
+   end
+
+ ////initialization
+ task initialize();
+  {clk,rst} = 0;
+ endtask
+
+ ////reset
+ task reset();
+  begin
+	@(negedge clk)
+	rst = 1'b1;
+	@(negedge clk)
+	rst = 1'b0;
+  end 
+ endtask
+
+///Stimulus
+ initial
+  begin
+   reset;
+   initialize;
+   
+//   reset;
+   
+ end  
+ initial #100 $finish;
+
+ endmodule  
+  
+  
+ 
+ 
